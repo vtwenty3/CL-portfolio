bb5b55b1804003bedb70cc6e52ee80a875eeb522
vtwenty3 <kisimovvalentin@gmail.com>
2023-08-15 13:54:28 +0100

    events functionality

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -2,23 +2,26 @@ import React from "react";
 import { AiOutlineCalendar } from "react-icons/ai";
 import { FaLocationArrow } from "react-icons/fa";
 import { MdArrowForwardIos } from "react-icons/md";
-
 import "./Event.css";
 
-export default function Event() {
+export default function Event({ date, month, title, location, link }) {
   return (
-    <div className="event">
-      <div className="event-date">
-        <h2>10</h2>
-        <span>sep</span>
+    <a href={link} target="_blank" rel="noopener noreferrer" className="event-link">
+
+      <div className="event">
+        <div className="event-date">
+          <h2>{date}</h2>
+          <span>{month}</span>
+        </div>
+        <div className="event-info">
+          <h2>{title}</h2>
+          <span>{location}</span>
+        </div>
+        <div className="event-arrow">
+          <MdArrowForwardIos className="event-arrow-icon" />
+        </div>
       </div>
-      <div className="event-info">
-        <h2>Event Title</h2>
-        <span>Edinburgh, La Belle Angele</span>
-      </div>
-      <div className="event-arrow">
-        <MdArrowForwardIos className="event-arrow" />
-      </div>
-    </div>
+    </a>
+
   );
 }
